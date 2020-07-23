1e07e8e59cad516c59a4b5975ea58e1326655646
adis0892 <amoda.dissanayake@sysco.com>
2020-07-23 15:48:14 -0500

    Condition modified

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,16 +1,15 @@
 import {AUTHENTICATION_NOT_REQUIRED_HEALTH_CHECK} from "../util/constants";
 import AuthService from '../service/auth/authService';
+import logger from "../util/logger";
 
-export async function authMiddleware (req, res, next) {
-    if (process.env.STAGE !== 'local' || !req.url.includes(AUTHENTICATION_NOT_REQUIRED_HEALTH_CHECK)) {
+export async function authMiddleware(req, res, next) {
+    if (process.env.STAGE !== 'local' && !(req.url === AUTHENTICATION_NOT_REQUIRED_HEALTH_CHECK)) {
+        logger.debug(`Sending to authenticate the request`);
         let authResponse = await AuthService.prepareToValidateToken(req, res);
-
-            if (authResponse.authenticated) {
-                res.locals.username = authResponse.username;
-                next();
-            }
-
-
+        if (authResponse.authenticated) {
+            res.locals.username = authResponse.username;
+            next();
+        }
     } else {
         next();
     }
