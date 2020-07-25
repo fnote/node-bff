bbe1f43fc467f8ed9243657bc02977b2127f49e8
adis0892 <amoda.dissanayake@sysco.com>
2020-07-24 23:22:32 -0500

    Await fetch

--- src/service/auth/authService.js
+++ src/service/auth/authService.js
@@ -3,6 +3,7 @@ import logger from "../../util/logger";
 import * as HttpStatus from "http-status-codes";
 import {createErrorResponse} from "../../mapper/responseMapper";
 import jwkToPem from "jwk-to-pem";
+import fetch from 'node-fetch';
 import {getAuthConfig} from "../../config/configs";
 
 const unauthenticatedReturn = {
@@ -20,9 +21,6 @@ class AuthService {
     prepareToValidateToken = async (req, res) => {
         try {
             let token = req.headers[this.authConfig.CONFIG.authTokenHeaderAttribute];
-
-            token = 'eyJraWQiOiJzYzVyUWJiMiszZnM3cmI5ODNBY0ZLZVhLNVJFdXgrMlpraUhlejE2ZlU0PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI3YzA2N2RkNy04ZWExLTQ1NGMtOTJiMC03Mjc2MGM3NGJhOTciLCJjb2duaXRvOmdyb3VwcyI6WyJ1cy1lYXN0LTFfSTVkdE5FOWQxX0F6dXJlQUQiXSwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJvcGVuaWQiLCJhdXRoX3RpbWUiOjE1OTQ5MjUyMzYsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0k1ZHRORTlkMSIsImV4cCI6MTU5NDkyODgzNiwiaWF0IjoxNTk0OTI1MjM2LCJ2ZXJzaW9uIjoyLCJqdGkiOiJhNTA2ZmNjZi1mMWE5LTQ5ZjQtYmIwOC1lMzJiMDg4MmIwMmYiLCJjbGllbnRfaWQiOiIydDE2b3FxZWEzZHM1YzJvcnAzcnBxbWU1YiIsInVzZXJuYW1lIjoiYXp1cmVhZF9hZGlzMDg5MiJ9.ZchsyUq1UUJRYvdQ1kKOqUCB2_U3pNmz5H3YP-pcKsqD4zsyfIvY7ZEZGviE-muxXBnssGEz4_ISJg4nr9DqvzCBnHOUzYMtRW-HVm20kboWDhH5JMpToA-m61Fqiyn7Y6NLwp6VlqIk3JQEbFxPJkLbhCv2uhQrFQp17CH1H-St5iwuy_2E0oo6WXR-mibVlkk1ARinfmT2dvMckpbxVMJPfV6yQxIqURHtRl89tQO-DYx6InugSm3shkjWCgCdr0EM-Hm1ig50b4dVbg0gPgAy7jt9LzO9FDO1xQ3RvHa8nTGiH3hNtss3i4_GDv_hjNaHlZLOih9npDdGawHldw'
-
 
             if (!token) {
                 let errorMessage = 'Access token is missing from header';
@@ -100,7 +98,7 @@ class AuthService {
         //Verify the signature of the JWT token to ensure it's really coming from our User Pool
         jwt.verify(token, pem, {algorithms: ["RS256"]}, (err, payload) => {
             if (err) {
-                logger.error('Token was failed to be verified with error:', err);
+                logger.error(`Token was failed to be verified with error: ${err}`);
                 this.sendUnauthenticatedErrorResponse(res, err.message);
                 returnObj = unauthenticatedReturn;
 
@@ -108,21 +106,25 @@ class AuthService {
                 let principalId = payload.sub;
                 let usernameWithAdTag = payload.username;
 
-                if (!principalId && !usernameWithAdTag) {
+                if (principalId && usernameWithAdTag) {
                     let username = usernameWithAdTag.split('_')[1];
-                    if (!username) {
+                    if (username) {
                         // Pass to the authorization
-                        logger.info(`The user's principal id:`, principalId, `username:`, username);
+                        logger.info(`The user's principal id: ${principalId} username: ${username}`);
                         returnObj = {
                             authenticated: true,
                             username: username
                         };
+                    } else {
+                        logger.error(`Username in the auth token is not in the expected format: ${username}`);
+                        this.sendUnauthenticatedErrorResponse(res, 'Username given in the authentication token is invalid');
                     }
+                } else {
+                    logger.error(`After token verification either principal id: ${principalId} or username: ${usernameWithAdTag} is not present`);
+                    this.sendUnauthenticatedErrorResponse(res, 'Required variables for authentication are invalid');
                 }
-                returnObj = unauthenticatedReturn;
             }
         });
-
         return returnObj;
     }
 
