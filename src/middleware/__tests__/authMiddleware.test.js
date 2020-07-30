e419cdd0a309208088769ca46e1d55e3756e3e14
adis0892 <amoda.dissanayake@sysco.com>
2020-07-30 14:21:21 -0500

    test cases added for auth middleware

---
+++ src/middleware/__tests__/authMiddleware.test.js
@@ -0,0 +1,45 @@
+/**
+ * Auth middleware unit tests
+ *
+ * @author: adis0892 on 26/07/20
+ **/
+
+import {authMiddleware} from "../authMiddleware";
+import {jest} from "@jest/globals";
+import {AUTHENTICATION_NOT_REQUIRED_HEALTH_CHECK} from "../../util/constants";
+
+jest.mock('../../service/auth/authService', () => ({
+    prepareToValidateToken: () => {
+        return {
+            authenticated: true,
+            username: 'test-username'
+        }
+    }
+}));
+
+const req = {
+}
+const res = {
+    locals: {}
+}
+const next = jest.fn();
+
+describe('Auth Middleware', () => {
+    test('should return error response as status error', async () => {
+
+        await authMiddleware(req, res, next);
+
+        expect(res.locals.username).toEqual('test-username');
+        expect(next).toHaveBeenCalled();
+    });
+
+    test('healthcheck', async () => {
+        const req = {
+            url: AUTHENTICATION_NOT_REQUIRED_HEALTH_CHECK
+        };
+
+        await authMiddleware(req, res, next);
+
+        expect(next).toHaveBeenCalled();
+    });
+});
