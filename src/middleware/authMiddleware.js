9737c6f78452d2c06c927580c89bf50a9fbff949
adis0892 <amoda.dissanayake@sysco.com>
2020-07-30 17:02:42 -0500

    Local changed

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import AuthService from '../service/auth/authService';
 import logger from "../util/logger";
 
 export async function authMiddleware(req, res, next) {
-    if (process.env.STAGE !== 'local' && !(req.url === AUTHENTICATION_NOT_REQUIRED_HEALTH_CHECK)) {
+    if (process.env.STAGE !== 'LOCAL' && !(req.url === AUTHENTICATION_NOT_REQUIRED_HEALTH_CHECK)) {
         logger.debug(`Sending to authenticate the request`);
         let authResponse = await AuthService.prepareToValidateToken(req, res);
         if (authResponse.authenticated) {
