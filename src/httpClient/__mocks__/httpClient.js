b2f98c965c3ef2e857b3fbda7d2d142994e93a17
adis0892 <amoda.dissanayake@sysco.com>
2020-08-06 16:09:13 -0500

    http client mock modified added stacktrace to errorhandler

--- src/httpClient/__mocks__/httpClient.js
+++ src/httpClient/__mocks__/httpClient.js
@@ -4,6 +4,17 @@
  * @author: gkar5861 on 23/06/20
  * */
 
+import {
+    cloudPricingMockRequest,
+    cloudPricingMockRequestForErrorScenario,
+    cloudPricingMockResponse,
+    cloudPricingMockResponseForAggregatedErrorPricingCall,
+    cloudPricingMockResponseForAggregatedPricingCall,
+    pricingDataMockRequest,
+    pricingDataMockRequestForErrorOnCloudPricingCall,
+    pricingDataMockRequestThrowErrorForCloudPricingCall,
+} from "../../config/test.config";
+
 const mockRequestBody = {
     fileNames: [
         'fileName1',
@@ -28,6 +39,16 @@ class HttpClient {
     static async makeRequest(method, URL, data) {
         if (JSON.stringify(data) === JSON.stringify(mockRequestBody)) {
             return mockResponse;
+        } else if (JSON.stringify(data) === JSON.stringify(cloudPricingMockRequest.body)) {
+            return cloudPricingMockResponse;
+        } else if (JSON.stringify(data) === JSON.stringify(cloudPricingMockRequestForErrorScenario.body)) {
+            throw new Error('test-error');
+        } else if (JSON.stringify(data) === JSON.stringify(pricingDataMockRequest)) {
+            return cloudPricingMockResponseForAggregatedPricingCall;
+        } else if (JSON.stringify(data) === JSON.stringify(pricingDataMockRequestForErrorOnCloudPricingCall)) {
+            return cloudPricingMockResponseForAggregatedErrorPricingCall;
+        } else if (JSON.stringify(data) === JSON.stringify(pricingDataMockRequestThrowErrorForCloudPricingCall)) {
+            throw new Error('test-error');
         }
     }
 }
