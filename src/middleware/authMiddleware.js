f59bc0c73d336f342f840047f1f1fb7074d19a7e
adis0892 <amoda.dissanayake@sysco.com>
2020-09-01 20:08:05 -0500

    login url and authenticated actions in one if condition catch(error) corrected

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -11,19 +11,17 @@ export async function authMiddleware(req, res, next) {
             const authResponse = await AuthService.prepareToValidateToken(req, res);
 
             /* Since login is browser calls from frontend, unauthenticated response should be res.direct to an login error,
-            can't directly send an error response like for other rest ajax calls
+            can't directly send an error response like for other rest ajax calls. For all the other calls check whether
+            the user is properly and if not send a http error response
              */
-            if (req.url === LOGIN_URL) {
+            if (req.url === LOGIN_URL || authResponse.authenticated) {
                 res.locals.authResponse = authResponse;
-                next();
-            } else if (authResponse.authenticated) {
-                res.locals.username = authResponse.username;
                 next();
             } else {
                 res.status(HttpStatus.UNAUTHORIZED).send(createErrorResponse('Unauthorized', 'User cannot be authenticated',
                     null, authResponse.cause));
             }
-        } catch (e) {
+        } catch (error) {
             const errMessage = 'Authorization interceptor failed';
             logger.error(`${errMessage}: ${error} cause: ${error.stack}`);
             res.status(HttpStatus.INTERNAL_SERVER_ERROR)
