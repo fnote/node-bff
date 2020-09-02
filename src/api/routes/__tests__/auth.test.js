03ef21bc4b90b5a9185bd067af840919ee8723e6
adis0892 <amoda.dissanayake@sysco.com>
2020-09-02 18:05:25 -0500

    More test cases added

--- src/api/routes/__tests__/auth.test.js
+++ src/api/routes/__tests__/auth.test.js
@@ -4,17 +4,25 @@
  * @author: adis0892 on 28/07/20
  * */
 
+jest.mock('../../../middleware/authMiddleware');
+let {authMiddleware} = require('../../../middleware/authMiddleware');
+
 import request from 'supertest';
 import * as HttpStatus from 'http-status-codes';
 import {jest} from '@jest/globals';
 import app from '../../../app';
 
-jest.mock('../../../middleware/authMiddleware', () => ({
-    authMiddleware: (req, res, next) => next(),
-}));
-jest.mock('../../../initializer', () => ({
-    initializer: (req, res, next) => next(),
-}));
+const userDetailsMockResponse =  {
+        "authorizedBunitList": [
+            "001",
+            "002",
+        ],
+        "email": "firstName.secondName@syscolabs.com",
+        "firstName": "firstName",
+        "jobTitle": "jobTitle",
+        "lastName": "secondName",
+        "username": "username"
+    }
 
 jest.mock('../../../initializer', () => ({
     initializer: (req, res, next) => next(),
@@ -24,6 +32,15 @@ const mockRequest = {};
 
 describe('routes: /auth', () => {
     test('get /login should redirect the user when authentication happens', async () => {
+        authMiddleware.mockImplementationOnce((req, res, next) => {
+                    res.locals.authResponse = {
+                        "authenticated": true,
+                        "cause": null,
+                    }
+                    next()
+
+        });
+
         await request(app.app)
             .get('/v1/pci-bff/auth/login')
             .send(mockRequest)
@@ -33,7 +50,37 @@ describe('routes: /auth', () => {
                 });
     });
 
+    test('get /login should redirect the user when authentication happens even when authenticate: false', async () => {
+
+        authMiddleware.mockImplementationOnce((req, res, next) => {
+            res.locals.authResponse = {
+                "authenticated": false,
+                "cause": null,
+                "username": "username",
+            }
+            next()
+
+        });
+
+        await request(app.app)
+            .get('/v1/pci-bff/auth/login')
+            .send(mockRequest)
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.status).toEqual(HttpStatus.MOVED_TEMPORARILY);
+            });
+    });
+
     test('get /logout should redirect user to the screen after logout', async () => {
+        authMiddleware.mockImplementationOnce((req, res, next) => {
+            res.locals.authResponse = {
+                "authenticated": true,
+                "cause": null,
+            }
+            next()
+
+        });
+
         await request(app.app)
             .get('/v1/pci-bff/auth/logout')
             .send(mockRequest)
@@ -43,5 +90,122 @@ describe('routes: /auth', () => {
                     expect(res.status).toEqual(HttpStatus.MOVED_TEMPORARILY);
                     expect(location).toEqual(expect.stringMatching('logout_uri='));
                 });
+    });
+
+    test('get /user-details should send successful response when auth response is successful', async () => {
+
+        authMiddleware.mockImplementationOnce((req, res, next) => {
+                    res.locals.authResponse = {
+                        "authenticated": true,
+                        "cause": null,
+                        "username": "username",
+                        "userDetailsData": {
+                            "authorizedBunitList": [
+                                "001",
+                                "002",
+                            ],
+                            "email": "firstName.secondName@syscolabs.com",
+                            "firstName": "firstName",
+                            "jobTitle": "jobTitle",
+                            "lastName": "secondName",
+                            "username": "username"
+                        },
+                    }
+                    next()
+
+        });
+
+        await request(app.app)
+            .get('/v1/pci-bff/auth/user-details')
+            .send(mockRequest)
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.status).toEqual(HttpStatus.OK);
+                expect(res.body).toEqual(userDetailsMockResponse);
+            });
+    });
+
+    test('get /user-details should send unauthorized response when auth response send "authenticated": false', async () => {
+
+        authMiddleware.mockImplementationOnce((req, res, next) => {
+            res.locals.authResponse = {
+                "authenticated": false,
+                "cause": null,
+                "username": "username",
+            }
+            next()
+
+        });
+
+        const errorResponse = {
+            "message": "User cannot be authenticated",
+           "status": "Unauthorized",
+        }
+
+        await request(app.app)
+            .get('/v1/pci-bff/auth/user-details')
+            .send(mockRequest)
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.status).toEqual(HttpStatus.UNAUTHORIZED);
+                expect(res.body).toEqual(errorResponse);
+            });
+    });
+
+    test('get /user-details should send unauthorized response when auth response send userDetailsData empty', async () => {
+
+        authMiddleware.mockImplementationOnce((req, res, next) => {
+            res.locals.authResponse = {
+                "authenticated": true,
+                "cause": null,
+                "username": "username",
+                "userDetailsData": {},
+            }
+            next()
+
+        });
+
+        const errorResponse = {
+            "cause": "User details are not present",
+          "message": "User cannot be authenticated",
+            "status": "Unauthorized",
+    }
+
+        await request(app.app)
+            .get('/v1/pci-bff/auth/user-details')
+            .send(mockRequest)
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.status).toEqual(HttpStatus.UNAUTHORIZED);
+                expect(res.body).toEqual(errorResponse);
+            });
+    });
+
+    test('get /user-details should send unauthorized response when auth response send userDetailsData is not defined', async () => {
+
+        authMiddleware.mockImplementationOnce((req, res, next) => {
+            res.locals.authResponse = {
+                "authenticated": true,
+                "cause": null,
+                "username": "username",
+            }
+            next()
+
+        });
+
+        const errorResponse = {
+            "cause": "User details are not present",
+            "message": "User cannot be authenticated",
+            "status": "Unauthorized",
+        }
+
+        await request(app.app)
+            .get('/v1/pci-bff/auth/user-details')
+            .send(mockRequest)
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.status).toEqual(HttpStatus.UNAUTHORIZED);
+                expect(res.body).toEqual(errorResponse);
+            });
     });
 });
--- src/middleware/__tests__/authMiddleware.test.js
+++ src/middleware/__tests__/authMiddleware.test.js
@@ -1,28 +1,40 @@
+import * as HttpStatus from "http-status-codes";
+
 /**
  * Auth middleware unit tests
  *
  * @author: adis0892 on 26/07/20
  * */
+jest.mock('../../service/auth/authService');
 
 import {jest} from '@jest/globals';
 import {authMiddleware} from '../authMiddleware';
-import {AUTHENTICATION_NOT_REQUIRED_HEALTH_CHECK} from '../../util/constants';
+import {AUTHENTICATION_NOT_REQUIRED_HEALTH_CHECK, LOGIN_URL, LOGOUT_URL} from '../../util/constants';
+import AuthService from '../../service/auth/authService';
+import httpMocks from 'node-mocks-http'
 
-jest.mock('../../service/auth/authService', () => ({
-    prepareToValidateToken: () => ({
-            authenticated: true,
-            username: 'test-username',
-        }),
-}));
+beforeEach(() => {
+    jest.clearAllMocks();
+});
 
 let req = {};
 const res = {
     locals: {},
+    body: {},
+    status: code => ({
+        send: message => ({code, message})
+    })
 };
 const next = jest.fn();
 
 describe('Auth Middleware', () => {
     test('should pass the username and call next when auth process completed', async () => {
+        await AuthService.prepareToValidateToken.mockImplementationOnce(() => ({
+                authenticated: true,
+                username: 'test-username',
+            })
+        );
+
         await authMiddleware(req, res, next);
 
         expect(res.locals.authResponse.username).toEqual('test-username');
@@ -37,5 +49,65 @@ describe('Auth Middleware', () => {
         await authMiddleware(req, res, next);
 
         expect(next).toHaveBeenCalled();
+    });
+
+    test('should skip authentication when logout endpoint is called', async () => {
+        req = {
+            url: LOGOUT_URL,
+        };
+
+        await authMiddleware(req, res, next);
+
+        expect(next).toHaveBeenCalled();
+    });
+
+    test('should skip authentication when login endpoint is called', async () => {
+        await AuthService.prepareToValidateToken.mockImplementationOnce(() => ({
+                authenticated: true,
+                username: 'test-username',
+            })
+        );
+
+        req = {
+            url: LOGIN_URL,
+        };
+
+        await authMiddleware(req, res, next);
+
+        expect(res.locals.authResponse.username).toEqual('test-username');
+        expect(next).toHaveBeenCalled();
+    });
+
+    test('should give unauthorized http response status when not login endpoint is called and authenticate: false', async () => {
+        await AuthService.prepareToValidateToken.mockImplementationOnce(() => ({
+                authenticated: false,
+                username: 'test-username',
+            })
+        );
+
+        const req = httpMocks.createRequest();
+        const res = httpMocks.createResponse();
+
+
+
+       await authMiddleware(req, res, next);
+
+        expect(res.statusCode).toEqual(HttpStatus.UNAUTHORIZED);
+
+    });
+
+    test('should send internal server error http status when prepareToValidateToken method throw an error', async () => {
+        const req = httpMocks.createRequest();
+        const res = httpMocks.createResponse();
+
+        await AuthService.prepareToValidateToken.mockImplementationOnce(() => {
+            throw Error('test-error');
+            }
+        );
+
+        await authMiddleware(req, res, next);
+
+        expect(res.statusCode).toEqual(HttpStatus.INTERNAL_SERVER_ERROR);
+
     });
 });
