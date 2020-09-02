455f9d23c39bf5783fcf06b76b45316314aaa461
adis0892 <amoda.dissanayake@sysco.com>
2020-09-01 20:19:20 -0500

    opco authorization validation

--- src/api/routes/pricing/pricingDataRoute.js
+++ src/api/routes/pricing/pricingDataRoute.js
@@ -9,14 +9,23 @@ import * as HttpStatus from 'http-status-codes';
 import AggregatedPricingDataService from '../../../service/pricing/aggregatedPricingDataService';
 import logger from '../../../util/logger';
 import {createErrorResponse} from '../../../mapper/responseMapper';
+import BusinessUnitAuthorization from '../../../service/auth/businessUnitAuthorization';
 
 export default () => {
     const cloudPricingRouter = new Router({mergeParams: true});
 
     cloudPricingRouter.post('/pricing-data', async (req, res) => {
         try {
-            const responseData = await AggregatedPricingDataService.getAggregatedPricingData(req);
-            res.status(HttpStatus.OK).send(responseData);
+            const isAuthorized = BusinessUnitAuthorization.isAuthorizedRequest(req, res);
+
+            if(isAuthorized) {
+                const responseData = await AggregatedPricingDataService.getAggregatedPricingData(req);
+                res.status(HttpStatus.OK).send(responseData);
+            } else {
+                res.status(HttpStatus.UNAUTHORIZED).send(createErrorResponse('Unauthorized',
+                    'User is not authorized to perform this action/ for the requested opco',
+                    null, 'User authorization validations failed'));
+            }
         } catch (error) {
             const errMessage = 'Error occurred in getting pricing related data';
             logger.error(`${errMessage}: ${error} cause: ${error.stack}`);
