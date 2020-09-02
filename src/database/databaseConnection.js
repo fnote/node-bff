1c3e9f7ce68437db5e13bf8e570494cfe94e1c35
adis0892 <amoda.dissanayake@sysco.com>
2020-09-01 20:13:30 -0500

    databaseConnection added

---
+++ src/database/databaseConnection.js
@@ -0,0 +1,30 @@
+/**
+ * Database connection
+ *
+ * @author: adis0892 on 27/08/20
+ * */
+
+import mysql from 'mysql2/promise'
+import {getSsmConfig} from "../service/aws/ssmService";
+
+
+const getConnection = async () => {
+    const host = await getSsmConfig('commonDbURL');
+    const user = await getSsmConfig('commonDbUsername');
+    const password = await getSsmConfig('commonDbPassword');
+
+    return mysql.createConnection({
+        host: host,
+        user: user,password,
+        database: 'CPPDB_Common01d'
+    });
+}
+
+const closeConnection = (conn) => {
+    conn.end();
+}
+
+export {
+    getConnection,
+    closeConnection
+};
