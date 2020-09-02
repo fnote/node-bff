f6ffed3b4ab18965cc4324cd96befd0d1522344d
adis0892 <amoda.dissanayake@sysco.com>
2020-09-01 20:11:51 -0500

    businessUnitDao added

---
+++ src/dao/businessUnitDao.js
@@ -0,0 +1,38 @@
+/**
+ * Business unit table DAO functions
+ *
+ * @author: adis0892 on 27/08/20
+ * */
+
+import {closeConnection, getConnection} from "../database/databaseConnection";
+import DatabaseException from "../exception/DatabaseException";
+import logger from '../util/logger';
+
+class BusinessUnitDao {
+    getBusinessUnitDetails = async () => {
+        let connection
+        try {
+            connection = await getConnection();
+
+            const [rows, fields] = await connection.execute('SELECT bunit_id, bunit_name, periscope_on from business_unit');
+
+            console.log(rows)
+
+            return rows;
+
+        } catch (e) {
+            const errorMessage = 'Failed to load business unit table data';
+            logger.error(`${errorMessage} due to: ${e}, stacktrace: ${e.stack}`);
+            throw new DatabaseException(
+                errorMessage,
+                e.message,
+            );
+        } finally {
+            if (connection) {
+                closeConnection(connection);
+            }
+        }
+    }
+}
+
+export default new BusinessUnitDao();
