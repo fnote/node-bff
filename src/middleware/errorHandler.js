0ee8a90e66e09dcba1ce27755516c7f3d530e266
adis0892 <amoda.dissanayake@sysco.com>
2020-10-23 16:35:19 -0500

    error handler to redirecting to login page

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -7,13 +7,16 @@ import {ERROR, LOGIN_URL} from '../util/constants';
 import logger from '../util/logger';
 import {INTERNAL_SERVER_ERROR} from "http-status-codes";
 import AuthenticateService from '../service/auth/authenticateService';
+import {getAuthConfig} from '../config/configs';
+const authConfig = getAuthConfig();
 
-export function handleError(err, req, res, next) {
+export function handleError(err, req, res) {
     logger.error(`Unhandled error occurred: ${err}, stacktrace: ${err.stack}`);
     const {cause, message} = err;
     if (req.url === LOGIN_URL) {
+        console.log('Redirecting......')
         res.locals.authResponse = AuthenticateService.sendUnauthenticatedErrorResponse(message);
-        next();
+        res.redirect(`${authConfig.CONFIG.loginRedirectionUrl}?login=error`);
     } else {
         res.status(INTERNAL_SERVER_ERROR).json({
             status: ERROR,
