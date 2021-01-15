908939659d6dc054e14d54bfb355fbe5b19786a3
gkar5861 <gothami.karunarathna@syscolabs.com>
2021-01-15 13:15:59 +0530

    batch service uit tests updated

--- src/service/__tests__/batchService.test.js
+++ src/service/__tests__/batchService.test.js
@@ -6,7 +6,7 @@
 import * as HttpStatus from 'http-status-codes';
 import BatchService from '../batch/batchService';
 import InvalidRequestException from '../../exception/invalidRequestException';
-import {INVALID_REQUEST_BODY} from '../../util/constants';
+import {EMPTY_REQUEST_BODY, INVALID_REQUEST_BODY} from '../../util/constants';
 import {
     mockBatchApiDownloadUrlRequest,
     mockBatchApiUploadUrlRequest,
@@ -20,36 +20,18 @@ jest.mock('../../httpClient/httpClient');
 
 
 describe('Batch Service', () => {
-    test('should generate input signed urls  when the request body is valid', async () => {
+    test('generateFileUploadSignedUrl should generate write signed urls  when the request body is valid', async () => {
         const response = await BatchService.generateFileUploadSignedUrl(mockBatchApiUploadUrlRequest);
         expect(response).toEqual(mockResponseSignedUrl);
     });
 
-    test('should generate output signed urls when the request body is valid', async () => {
+    test('generateFileDownloadSignedUrl should generate read signed urls when the request body is valid', async () => {
         const response = await BatchService.generateFileDownloadSignedUrl(mockBatchApiDownloadUrlRequest);
         expect(response).toEqual(mockResponseSignedUrl);
     });
 
-    test('should throw exception  when the request body is empty', async () => {
-        const emptyRequestBody = {};
-        await expect(BatchService.generateFileUploadSignedUrl(emptyRequestBody)).rejects
-            .toThrowError(new InvalidRequestException(
-                INVALID_REQUEST_BODY,
-                HttpStatus.BAD_REQUEST,
-            ));
-    });
-
-    test('should throw exception  when the request body is invalid', async () => {
-        const invalidRequestBody = {};
-        await expect(BatchService.generateFileUploadSignedUrl(invalidRequestBody)).rejects
-            .toThrowError(new InvalidRequestException(
-                INVALID_REQUEST_BODY,
-                HttpStatus.BAD_REQUEST,
-            ));
-    });
-
-    test('should generate list of files  when the source is valid', async () => {
-        const response = await BatchService.getBatchJobs('test1234', 'pageSize=,offSet="",searchQuery=""');
+    test('getBatchJobs should generate list of files  when the source is valid', async () => {
+        const response = await BatchService.getBatchJobs('test1234', '?pageSize=10&offSet=10');
         expect(response).toEqual(mockResponseJobList);
     });
 
