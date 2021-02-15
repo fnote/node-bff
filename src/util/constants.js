1ce1735515f9cf5d0a1f5c49ec1834f94aa3f8af
gkar5861 <gothami.karunarathna@syscolabs.com>
2021-02-15 16:22:56 +0530

    updated the PCI version

--- src/util/constants.js
+++ src/util/constants.js
@@ -4,7 +4,7 @@
  * */
 
 export const CLOUD_PCI_BFF = 'Cloud PCI BFF';
-export const CLOUD_PCI_BFF_VERSION = '1.0.0';
+export const CLOUD_PCI_BFF_VERSION = '1.4.0';
 export const SUCCESS = 'success';
 export const ERROR = 'error';
 
