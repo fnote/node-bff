df0abd853d38a860198cc4d0d0532f8682a68a6e
rshe2081 <Rashmi.Shehana@syscolabs.com>
2021-06-15 10:33:37 +0530

    [ TRSP-107] add mock data to seperate file

---
+++ src/service/cipzMockData.js
@@ -0,0 +1,18 @@
+export const seedGetItemAttributeGroupMockResponse = {
+    data: {
+        attributeGroups:[
+            {
+                name:"MILK",
+                id:12213
+            },
+            {
+                name:"FOOD STORAGE BAGS/SANDWICH BAGS/PAN LINERS",
+                id:16892
+            },
+            {
+                name:"FROZEN PASTA",
+                id:12341
+            }
+        ]
+    },
+};
