349d446ac12105d1b0282f07eac940abb4d1a875
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2021-06-16 21:06:47 +0530

    [TRSP-107] Update eerror codes

--- src/service/seed/seedDataService.js
+++ src/service/seed/seedDataService.js
@@ -8,7 +8,11 @@ import {getSeedApiConfig} from '../../config/configs';
     APPLICATION_JSON, CORRELATION_ID_HEADER,
     //  HTTP_POST
  } from '../../util/constants';
- import { SEED_API_ITEM_ATT_GROUP_FETCH_ERROR_CODE } from '../../exception/exceptionCodes';
+ import {
+     SEED_API_ITEM_ATT_GROUP_FETCH_ERROR_CODE,
+     SEED_API_SEARCH_BY_CUSTOMER_ERROR_CODE,
+     SEED_API_SEARCH_BY_CUSTOMER_GROUP_ERROR_CODE
+    } from '../../exception/exceptionCodes';
  import {
      seedGetItemAttributeGroupMockResponse,
      searchByCustomerMockResponse,
@@ -39,17 +43,25 @@ import {getSeedApiConfig} from '../../config/configs';
     }
 
     async getPriceZoneDetailsForCustomerAndItemAttributeGroup(req) {
-        // const headers = await this.constructHeaders();
-        // const reqUrl = this.seedApiConfig.CONFIG.seedApiBaseUrl + this.seedApiConfig.CONFIG.getCustomerAndItemAttributeGroupsEndpoint;
-        // return httpClient.makeRequest(HTTP_POST, reqUrl, req.body, headers);
-        return (searchByCustomerMockResponse.data);
+        try {
+            // const headers = await this.constructHeaders();
+            // const reqUrl = this.seedApiConfig.CONFIG.seedApiBaseUrl + this.seedApiConfig.CONFIG.getCustomerAndItemAttributeGroupsEndpoint;
+            // return httpClient.makeRequest(HTTP_POST, reqUrl, req.body, headers);
+            return (searchByCustomerMockResponse.data);
+        } catch (error) {
+            throw new SeedApiDataFetchException(error, ERROR_IN_GETTING_SEED_SEARCH_RESULTS, SEED_API_SEARCH_BY_CUSTOMER_ERROR_CODE);
+        }
     }
 
     async getPriceZoneDetailsForCustomerGroupAndItemAttributeGroup(req) {
-        // const headers = await this.constructHeaders();
-        // const reqUrl = this.seedApiConfig.CONFIG.seedApiBaseUrl + this.seedApiConfig.CONFIG.getCustomerGroupAndItemAttributeGroupsEndpoint;
-        // return httpClient.makeRequest(HTTP_POST, reqUrl, req.body, headers);
-        return (searchByCustomerGroupMockResponse.data);
+        try {
+            // const headers = await this.constructHeaders();
+            // const reqUrl = this.seedApiConfig.CONFIG.seedApiBaseUrl + this.seedApiConfig.CONFIG.getCustomerGroupAndItemAttributeGroupsEndpoint;
+            // return httpClient.makeRequest(HTTP_POST, reqUrl, req.body, headers);
+            return (searchByCustomerGroupMockResponse.data);
+        } catch (error) {
+            throw new SeedApiDataFetchException(error, ERROR_IN_GETTING_SEED_SEARCH_RESULTS, SEED_API_SEARCH_BY_CUSTOMER_GROUP_ERROR_CODE);
+        }
     }
 
     async sendGetRequest(reqUrl, headers) {
