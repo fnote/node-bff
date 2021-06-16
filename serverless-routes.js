2b6ef64ca50cd752c371700d54d0b8981aa2710d
rshe2081 <Rashmi.Shehana@syscolabs.com>
2021-06-16 10:33:00 +0530

    [ TRSP-107] get submitted requests endpoint

--- serverless-routes.js
+++ serverless-routes.js
@@ -16,7 +16,8 @@ const routes = [
   { path: '/auth/logout', method: GET },
   { path: '/auth/user-details', method: GET },
   { path: '/pricing/pricing-data', method: POST },
-  { path: '/price-zone-reassignment/item-attribute-groups', method: GET}
+  { path: '/price-zone-reassignment/item-attribute-groups', method: GET},
+  { path: '/price-zone-reassignment/pz-update-requests', method: GET}
 ];
 
 module.exports = () => {
