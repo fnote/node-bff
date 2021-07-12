c27a63daddf17047f18655edcdae054df4c902fc
rshe2081 <Rashmi.Shehana@syscolabs.com>
2021-07-12 14:51:49 +0530

    [TRSP-113] unit test coverage increasing

--- src/service/__tests__/seedDataService.test.js
+++ src/service/__tests__/seedDataService.test.js
@@ -1,13 +1,14 @@
- import {jest} from '@jest/globals';
- import seedService from '../seed/seedService';
- import {seedGetItemAttributeGroupMockResponse} from '../../config/test.config.pzreassignment';
- import SeedApiDataFetchException from '../../exception/seedApiDataFechException';
- import { CIPZ_SEED_VALIDATION_ERROR_CODES } from '../../exception/exceptionCodes';
+import { jest } from '@jest/globals';
+import seedService from '../seed/seedService';
+import { seedGetItemAttributeGroupMockResponse } from '../../config/test.config.pzreassignment';
+import SeedApiDataFetchException from '../../exception/seedApiDataFechException';
+import { CIPZ_SEED_VALIDATION_ERROR_CODES } from '../../exception/exceptionCodes';
+import apiCentralClient from '../../httpClient/apiCentralClient';
 
- jest.mock('../../httpClient/httpClient');
- jest.mock('../../util/accessTokenGenerator');
+jest.mock('../../httpClient/httpClient');
+jest.mock('../../util/accessTokenGenerator');
 
- jest.mock('../../config/configs', () => ({
+jest.mock('../../config/configs', () => ({
     getSeedApiBaseUrl: () => 'http://localhost:3000/services/v1/edwp',
     getSeedApiConfig: () => ({
         CONFIG: {
@@ -19,22 +20,54 @@
     }),
 }));
 
- describe('Seed API Data Service', () => {
-     test('should generate the correct response when flow is correct', async () => {
-         const response = await seedService.getSeedItemAttributeGroupsData();
-         console.log(response);
-         expect(response.data).toEqual(seedGetItemAttributeGroupMockResponse.data);
-     });
+describe('Seed API Data Service', () => {
+    test('should generate the correct response when flow is correct', async () => {
+        const response = await seedService.getSeedItemAttributeGroupsData();
+        expect(response.data).toEqual(seedGetItemAttributeGroupMockResponse.data);
+    });
 
-     test('should generate the error response', async () => {
+    test('should generate the error response', async () => {
         const response = new SeedApiDataFetchException('Test Error',
-         'Error', CIPZ_SEED_VALIDATION_ERROR_CODES.PRICE_ZONE_REASSIGNMENT_INVALID_UPDATE_PAYLOAD_ERROR_CODE);
+            'Error', CIPZ_SEED_VALIDATION_ERROR_CODES.PRICE_ZONE_REASSIGNMENT_INVALID_UPDATE_PAYLOAD_ERROR_CODE);
         expect(response.getStatus()).toEqual('Error');
     });
 
     test('should generate the error response', async () => {
         const response = new SeedApiDataFetchException('Test Error',
-         null, CIPZ_SEED_VALIDATION_ERROR_CODES.PRICE_ZONE_REASSIGNMENT_INVALID_UPDATE_PAYLOAD_ERROR_CODE);
+            null, CIPZ_SEED_VALIDATION_ERROR_CODES.PRICE_ZONE_REASSIGNMENT_INVALID_UPDATE_PAYLOAD_ERROR_CODE);
         expect(response.getStatus()).toEqual(-1);
+    });
+
+    test('should generate the error response if errorDetails are not present', async () => {
+        jest.spyOn(apiCentralClient, 'get').mockImplementation(() => {
+            throw new Error();
+        });
+        try {
+            await seedService.getSeedItemAttributeGroupsData();
+        } catch (e) {
+            expect(e.errorCode).toEqual(5006);
+        }
+    });
+
+    test('should generate the error response', async () => {
+        jest.spyOn(apiCentralClient, 'post').mockImplementation(() => {
+            throw new Error();
+        });
+        try {
+            await seedService.getPriceZoneDetailsForCustomerAndItemAttributeGroup('');
+        } catch (e) {
+            expect(e.errorCode).toEqual(5006);
+        }
+    });
+
+    test('should generate the error response', async () => {
+        jest.spyOn(apiCentralClient, 'post').mockImplementation(() => {
+            throw new Error();
+        });
+        try {
+            await seedService.getPriceZoneDetailsForCustomerGroupAndItemAttributeGroup('');
+        } catch (e) {
+            expect(e.errorCode).toEqual(5006);
+        }
     });
  });
